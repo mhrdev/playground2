a6b55de519d11df7b3668f8e27fd897afa4f9135
a-sep <artur.sep@gmail.com>
2016-11-05 06:25:37 +0100

    added new inveder img, removed b.js,

--- src/super.js
+++ src/super.js
@@ -1,2 +1,7 @@
 var a=1;
 
+// TODO collision
+if (this.pos.y === spaceship.pos.y) {
+    if (this.pos.x >= spaceship.pos.x && this.pos.x <= spaceship.pos.x + 100)
+        console.log('OK trafiony', spaceship.pos.x, spaceship.pos.y);
+    }
