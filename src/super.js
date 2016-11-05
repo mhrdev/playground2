36f874ee9a36d14d5cee0e40d475261bc686e0cc
a-sep <artur.sep@gmail.com>
2016-11-05 09:20:27 +0100

    nowa zmienna b

--- src/super.js
+++ src/super.js
@@ -1,10 +1,13 @@
 var a = 1;
-
+var b = 2;
 function info() {
     for (var i = 10; i > a; i--) {
         console.log('info',i,a);
     }
 }
+
+
+
 
 document.getElementById("startButton").addEventListener("click", function(event) {
     // display the current click count inside the clicked div
